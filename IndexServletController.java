21e93aa0bc9bbe7461e663b91dd8f6b0a5bf5503
ARIKARAN S <91272168+Arikaran-13@users.noreply.github.com>
2022-07-10 15:11:56 +0530

    session for eah request

--- IndexServletController.java
+++ IndexServletController.java
@@ -1,17 +1,20 @@
 package com.arikaran;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexServletController {
 	
-	@RequestMapping("index")
-	
-	public String index() {
-		System.out.println("Hello");
+     @RequestMapping("index")
+    // @ResponseBody
+	public String index(String name,HttpSession session) {
+    	 System.out.println("HEllo world");
+    	 session.setAttribute("username", name);
 		return "index.jsp";
 	}
-
 }
